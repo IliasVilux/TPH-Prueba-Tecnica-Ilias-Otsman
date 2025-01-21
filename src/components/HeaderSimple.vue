b4cf5bbcf0d79965942665ed52dc01f279564db9
IliasVilux <iliasotsman@gmail.com>
2025-01-21 18:40:39 +0100

    feat: :art: Create 'HeaderSimple' component to redirecto from auth views

---
+++ src/components/HeaderSimple.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="position-absolute text-center w-100 z-1">
+    <RouterLink :to="{ name: 'home' }">
+      <h2 class="fs-4 m-0 text-white mt-4">Inicio</h2>
+    </RouterLink>
+  </header>
+</template>
